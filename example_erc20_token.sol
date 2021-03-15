e57d0e0087b0552ec63e8d0b76e2327c60be7a5f
nvasilij1 <nvasilij1@yandex.ru>
2021-03-15 08:35:58 +0300

    Example ERC20 token only owner emit

--- example_erc20_token.sol
+++ example_erc20_token.sol
@@ -1,6 +1,59 @@
 pragma solidity 0.5.12;
+contract Ownable {
+    address private _owner;
 
-contract exampleERC20Test{
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+     * account.
+     */
+    constructor () internal {
+        _owner = msg.sender;
+        emit OwnershipTransferred(address(0), _owner);
+    }
+
+    /**
+     * @return the address of the owner.
+     */
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(isOwner());
+        _;
+    }
+
+    /**
+     * @return true if `msg.sender` is the owner of the contract.
+     */
+    function isOwner() public view returns (bool) {
+        return msg.sender == _owner;
+    }
+
+    /**
+     * @dev Allows the current owner to transfer control of the contract to a newOwner.
+     * @param newOwner The address to transfer ownership to.
+     */
+    function transferOwnership(address newOwner) public onlyOwner {
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers control of the contract to a newOwner.
+     * @param newOwner The address to transfer ownership to.
+     */
+    function _transferOwnership(address newOwner) internal {
+        require(newOwner != address(0));
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
+contract exampleERC20Test is Ownable{
     string public constant name = "MyExampleToken";
     string public constant symbol = "MET"; // MyExampleToken
     uint8 public constant decimal = 2; //так то 18 знаков после запятой но для теста 2
@@ -16,7 +69,7 @@ contract exampleERC20Test{
     event Approval(address indexed _from, address indexed _to, uint _value);
     
     
-    function mint(address _to, uint _value) public {
+    function mint(address _to, uint _value) onlyOwner public { //дополнение, только владелец контракта может выпустить токены
         require(totalSupply + _value >= totalSupply && balances[_to] + _value >= balances[_to]); //проверка на переполнение (по хорошему надо библиотеку)
         balances[_to] += _value;
         totalSupply += _value;
